26bc8305d282eb8a33151a3d18b59bbd1635b730
uzairhafeez <muh3@njit.edu>
2017-05-08 14:49:39 -0400

    Automated deployment to target

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -28,60 +28,64 @@ function updatePackage($request)
         return $response;
 }
 
+function currentPackage($request)
+{
+	$db = new DeployFuncLib();
+	$db->connect();
+ 	$response = $db->getCurrentPackage($request['packageName']);
+	return $response;
+}
+
 function deployPackage($request)
 {
 	
 	$json = file_get_contents('./virtualMachines.json');
 	$decodedJson = json_decode($json, true);
 
+# Grab routing information based for the target machine based on client request
 	$targetMachine = ($decodedJson[$request['package']][$request['tier']] );
 	$targetIp = $targetMachine['ip'];
+	$targetPass = $targetMachine['pass'];
 
+# Determine which package to send to the target 	
 	$db = new DeployFuncLib();
 	$db->connect();
 	$packageNum = $db->nextPackageChecker($request['packageName']);
 	$packageNum -= 1;
 	$package = $request['packageName'] . $packageNum;
 
-	$scpPackage = 'scp -r /home/uzair/Packages/' . $package . '.tar.gz ' . $targetMachine['username'] . '@' . $targetIp . ':/home/' . $targetMachine['username'] . '/temp';
+	$scpPackage = 'sshpass -p ' . $targetPass . ' scp -r /home/uzair/Packages/' . $package . '.tar.gz ' . $targetMachine['username'] . '@' . $targetIp . ':/home/' . $targetMachine['username'] . '/Packages';
 	
 	shell_exec($scpPackage);
 	
+# Changes exchanged based on the destination of the package to trigger 
+# target machines installer
 
 	if(($request['package'] == 'BE') && ($request['tier']  == 'qa'))
 		$packageExchange = "backendQA";
-	
 	elseif(($request['package'] == 'BE') && ($request['tier']  == 'prod'))
                 $packageExchange = "backendProd";
-
 	elseif(($request['package'] == 'FE') && ($request['tier']  == 'qa'))
                 $packageExchange = "frontendQA";
-
 	elseif(($request['package'] == 'FE') && ($request['tier']  == 'prod'))
                 $packageExchange = "frontendProd";
-
 	elseif(($request['package'] == 'API') && ($request['tier']  == 'qa'))
                 $packageExchange = "apiQA";
-
-        elseif(($request['package'] == 'API') && ($request['tier']  == 'prod'))
+	elseif(($request['package'] == 'API') && ($request['tier']  == 'prod'))
                 $packageExchange = "apiProd";	
-
-	
 	else
 		$packageExchange = "deployServer";
 
-	$client = new rabbitMQClient("deployRabbitServer.ini", $packageExchange);
+# Send Client request to the target machine
+
+	$client = new rabbitMQClient("deployRabbitServer.ini", $packageExchange);	
 	$deployReq = array();
-       //	$deployReq['type'] = 'deployPackage';
 	$deployReq['type'] = $request['package'] . $request['tier'];
 	$deployReq['version'] = $packageNum;
 	$deployReq['packageName'] = $request['packageName'];
 	$deployReq['packageTar'] = $request['packageName'] . $packageNum . ".tar.gz";
 
-	$client->publish($deployReq);	
-
-
-  
+	$client->publish($deployReq);	  
 }
 
 function rollbackPackage($request)
@@ -92,6 +96,7 @@ function rollbackPackage($request)
 
         $targetMachine = ($decodedJson[$request['package']][$request['tier']] );
         $targetIp = $targetMachine['ip'];
+	$targetPass = $targetMachine['pass'];
 
         $db = new DeployFuncLib();
         $db->connect();
@@ -99,7 +104,7 @@ function rollbackPackage($request)
         $packageNum = $packageNum - 2;
         $package = $request['packageName'] . $packageNum;
         
-        $scpPackage = 'scp -r /home/uzair/Packages/' . $package . '.tar.gz ' . $targetMachine['username'] . '@' . $targetIp . ':/home/' . $targetMachine['username'] . '/temp';
+        $scpPackage = 'sshpass -p ' . $targetPass . ' scp -r /home/uzair/Packages/' . $package . '.tar.gz ' . $targetMachine['username'] . '@' . $targetIp . ':/home/' . $targetMachine['username'] . '/temp';
 	shell_exec($scpPackage);
 	
 	$client = new rabbitMQClient("deployRabbitServer.ini","deployServer");
@@ -135,6 +140,8 @@ function requestProcessor($request)
       return updatePackage($request);
     case "rollback":
       return rollbackPackage($request);	
+    case "currentPackage":
+      return currentPackage($request);
   }
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
